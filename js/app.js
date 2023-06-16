47df5afa924d089f192e7f2b6c6ee8dab391824c
object-ions <moshikoatia@gmail.com>
2023-06-16 14:13:25 -0700

    pass the first test and add more functionality to the app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-// Buisness Logic:
+// Buisness Logic:pepperoni
+const basicPizzaSmall = 10;
+const basicPizzaMedium = 12;
+const basicPizzaLarge = 13;
+const mushrooms = 0.5;
+const onions = 0.5;
+const peppers = 0.5;
+const olives = 0.5;
+const tomatoes = 0.5;
+const pineapple = 1;
+const artichoke = 1;
+const pepperoni = 1.5;
 
-function Pizza() {
+function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 };
 
+function calcPrice(size, toppings) {
+  let calcToppings;
+
+  return size + toppings
+}
 
 // UI Logic:
